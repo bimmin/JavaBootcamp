c89ee361b8226577a917558a7eb3e0a9dc75f20a
Siriwimon Nanthaban <siriwimon@Siriwimon.local>
2021-02-09 13:18:05 +0700

    level 1 kata - classess

--- 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
+++ 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
@@ -6,8 +6,15 @@ package bootcamp.kata.process;
 import bootcamp.kata.data.Params;
 import bootcamp.kata.data.Results;
 
+import java.math.BigDecimal;
+
 public class Processor {
     public Results process(final Params params) {
-        return null; // FIXME
+        BigDecimal sum = params.getX().add(params.getY());
+        BigDecimal difference = params.getX().subtract(params.getY());
+        BigDecimal product = params.getX().multiply(params.getY());
+        BigDecimal quotiant = params.getX().divide(params.getY());
+
+        return new Results(sum, difference, product, quotiant);
     }
 }
